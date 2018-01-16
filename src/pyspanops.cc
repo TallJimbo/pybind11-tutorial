57adfc959950001af07e03c5292e00b2219c4dab
Jim Bosch <jbosch@astro.princeton.edu>
2018-01-16 14:36:48 -0500

    Interval pybind11: class wrappers only.

--- src/pyspanops.cc
+++ src/pyspanops.cc
@@ -1,9 +1,23 @@
 #include "pybind11/pybind11.h"
+#include "spanops.h"
 
 namespace py = pybind11;
 
+using namespace pybind11::literals;
+
+namespace spanops {
+namespace {
+
+void declareInterval(py::module & mod) {
+    py::class_<Interval> cls(mod, "Interval");
+}
+
+} // anonymous
+} // spanops
+
 
 PYBIND11_MODULE(spanops, m) {
+    spanops::declareInterval(m);
 #ifdef VERSION_INFO
     m.attr("__version__") = VERSION_INFO;
 #else
